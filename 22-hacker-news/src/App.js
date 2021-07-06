d75db5e24c48fbe3dc72c9bea028029186524b2c
Raymond Oluoch <rasholuoch@gmail.com>
2021-07-06 18:59:18 +0300

    adding hacker new project

---
+++ 22-hacker-news/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import SearchForm from './SearchForm'
+import Stories from './Stories'
+import Buttons from './Buttons'
+
+const App = () => {
+  return (
+    <>
+      <SearchForm />
+      <Buttons />
+      <Stories />
+    </>
+  )
+}
+
+export default App
