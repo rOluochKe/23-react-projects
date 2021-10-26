a1a859c7fd48d43098a3e0849496f45536b63063
roluochKe <rasholuoch@gmail.com>
2021-10-26 15:04:57 +0300

    fixing bugs

--- travelbnb/src/App.js
+++ travelbnb/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { CssBaseline, Grid } from '@material-ui/core'
-import { getPlacesData, getWeatherData } from './api'
 
+import { getPlacesData, getWeatherData } from './api/'
 import Header from './components/Header/Header'
 import List from './components/List/List'
 import Map from './components/Map/Map'
@@ -10,16 +10,16 @@ const App = () => {
   const [type, setType] = useState('restaurants')
   const [rating, setRating] = useState('')
 
-  const [places, setPlaces] = useState([])
-  const [childCliked, setChildClicked] = useState(null)
-
   const [coordinates, setCoordinates] = useState({})
-  const [bounds, setBounds] = useState({})
+  const [bounds, setBounds] = useState(null)
 
   const [weatherData, setWeatherData] = useState([])
   const [filteredPlaces, setFilteredPlaces] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [places, setPlaces] = useState([])
+
   const [autocomplete, setAutocomplete] = useState(null)
+  const [childClicked, setChildClicked] = useState(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -50,7 +50,7 @@ const App = () => {
         setIsLoading(false)
       })
     }
-  }, [type, bounds])
+  }, [bounds, type])
 
   const onLoad = (autoC) => setAutocomplete(autoC)
 
@@ -69,7 +69,7 @@ const App = () => {
         <Grid item xs={12} md={4}>
           <List
             isLoading={isLoading}
-            childCliked={childCliked}
+            childClicked={childClicked}
             places={filteredPlaces.length ? filteredPlaces : places}
             type={type}
             setType={setType}
@@ -77,14 +77,22 @@ const App = () => {
             setRating={setRating}
           />
         </Grid>
-
-        <Grid item xs={12} md={8}>
+        <Grid
+          item
+          xs={12}
+          md={8}
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
           <Map
-            setCoordinates={setCoordinates}
+            setChildClicked={setChildClicked}
             setBounds={setBounds}
+            setCoordinates={setCoordinates}
             coordinates={coordinates}
             places={filteredPlaces.length ? filteredPlaces : places}
-            setChildClicked={setChildClicked}
             weatherData={weatherData}
           />
         </Grid>
