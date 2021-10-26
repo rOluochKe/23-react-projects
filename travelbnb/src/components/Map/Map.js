dcf665c2607b4707ad0232b86caf23d667037b2e
roluochKe <rasholuoch@gmail.com>
2021-10-26 13:03:28 +0300

    fetching and displaying places details

--- travelbnb/src/components/Map/Map.js
+++ travelbnb/src/components/Map/Map.js
@@ -6,21 +6,23 @@ import Rating from '@material-ui/lab'
 
 import useStyles from './styles'
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
   const classes = useStyles()
   const isMobile = useMediaQuery('(min-width:600px)')
-  const coordinates = { lat: 0, lng: 0 }
 
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyA7M9uFUKc9elIatR5RBZ6i7Rxoj9-1Aro' }}
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
         defaultCenter={coordinates}
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
         options={''}
-        onChange={''}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng })
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw })
+        }}
         onChildClick={''}
       ></GoogleMapReact>
     </div>
